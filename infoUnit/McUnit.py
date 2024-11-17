9e2e6bbbc6a1fcccfe16257ff2a6bf1b7a9ec8cd
wcc11404 <chengyu_3663@126.com>
2024-11-17 20:09:40 +0800

    更改底层数据结构

--- infoUnit/McUnit.py
+++ infoUnit/McUnit.py
@@ -1,14 +1,17 @@
 import json
 
-class marketcondition:
-    def __init__(self, open, close, high, low, pre_close, change, pct_chg, vol, amount):
+"""
+Market Condition
+"""
+class McUnit:
+    def __init__(self, open, close, high, low, pre_close, change, pct_change, vol, amount):
         self.open = open            # 开盘价
         self.close = close          # 收盘价
         self.high = high            # 最高价
         self.low = low              # 最低价
         self.pre_close = pre_close  # 昨收价
         self.change = change        # 涨跌额
-        self.pct_chg = pct_chg      # 涨跌幅
+        self.pct_change = pct_change    # 涨跌幅
         self.vol = vol              # 成交量
         self.amount = amount        # 成交额
 
@@ -20,7 +23,7 @@ class marketcondition:
             "最低价": self.low,
             "昨收价": self.pre_close,
             "涨跌额": self.change,
-            "涨跌幅": self.pct_chg,
+            "涨跌幅": self.pct_change,
             "成交量": self.vol,
             "成交额": self.amount,
         }
@@ -31,3 +34,5 @@ class marketcondition:
     def show(self):
         for k, v in self.to_dict().items():
             print("{}\t{}".format(k, v))
+
+default_mc_unit = McUnit(-1, -1, -1, -1, -1, -1, -1, -1, -1)
